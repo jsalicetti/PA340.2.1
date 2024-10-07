c325aed524e49ada92cc21bc4f3328883553ece2
jsalicetti <jessicasalicetti@yahoo.com>
2024-10-07 11:59:37 -0400

    completed

---
+++ PythonNumberDatatype.py
@@ -0,0 +1,135 @@
+
+# PROBLEM 1
+# declare two integer variables 
+num1 = 30
+num2 = 5
+
+# add the two integers together and assign the sum to a variable
+sum_result = num1 + num2
+
+# print
+print("The sum of", num1, "and", num2, "is:", sum_result)
+
+
+
+# PROBLEM 2
+# declare two float variables (decimal numbers)
+num1 = 20.5
+num2 = 11.3
+
+# add the two floats together and assign the sum to a variable
+sum_result = num1 + num2
+
+# print
+print("The sum of", num1, "and", num2, "is:", sum_result)
+
+
+
+# PROBLEM 3
+# declare an integer variable
+int  = 22
+
+# declare a float variable 
+float = 2.2
+
+# add the integer and float together and assign the sum to a variable
+sum_result = int  + float
+
+# print
+print("The sum of", int , "and", float, "is:", sum_result)
+
+
+
+# PROBLEM 4
+# declare two integer variables 
+num1 = 9
+num2 = 54
+
+# get the larger and smaller numbers
+larger = max(num1, num2)
+smaller = min(num1, num2)
+
+# divide the larger number by the smaller number 
+result = larger / smaller
+
+# print
+print("Dividing", larger, "by", smaller, "is:", result)
+
+
+
+# PROBLEM 5
+# declare two float variables
+float1 = 2.55
+float2 = 5.22
+
+# get the larger and smaller numbers
+larger = max(float1, float2)
+smaller = min(float1, float2)
+
+# divide the larger number by the smaller number 
+result = larger / smaller
+
+# print
+print("The result of dividing", larger, "by", smaller, "is:", result)
+
+
+
+# PROBLEM 6
+# declare variables 
+coffee = 1.50
+cappuccino = 5.25
+green_tea = 4.75
+
+# print
+# print("Cafe Prices:")
+print("Coffee: $", coffee)
+print("Cappuccino: $", cappuccino)
+print("Green tea: $", green_tea)
+
+
+
+# PROBLEM 7
+# declare variables 
+coffee = 1.50
+cappuccino = 5.25
+green_tea = 4.75
+
+# declare the quantities
+coffee_order = 3
+cappuccino_order = 4
+green_tea_order = 2
+
+# subtotal
+subtotal = (coffee * coffee_order) + \
+           (cappuccino * cappuccino_order) + \
+           (green_tea * green_tea_order)
+
+# declare the sales tax_price rate (e.g., 3%)
+SALES_TAX = 0.03
+
+# total sale amount
+totalSale = subtotal + (subtotal * SALES_TAX)
+
+# print
+print("Subtotal: $", round(subtotal, 2))
+print("Total Sale (including tax): $", round(totalSale, 2))
+
+
+# PROBLEM 8
+# define the radius
+radius = 63
+
+# define the value of pi
+pi = 3.14
+
+# area of the circle
+area = pi * (radius ** 2)
+
+# print
+print("The area of the circle with radius", radius, "is:", area)
+
+
+
+
+
+
